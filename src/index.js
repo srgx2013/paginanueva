84f139c924eb3dfdc4d675d5a3cf05379a748e6d
srgx2013 <srgx2012@gmail.com>
2022-12-15 10:25:59 -0600

    primer intento

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const seleccionarBotonRespuesta = document.getElementById('seleccionar-boton-respuesta')
+
+const seleccionarBotonRespuesta2 = document.getElementById('seleccionar-boton-respuesta2')
+const seleccionarBotonRespuesta3 = document.getElementById('seleccionar-boton-respuesta3')
+seleccionarBotonRespuesta.onclick = function() {
+  alert('Seleccionaste respuesta')
+}
+
